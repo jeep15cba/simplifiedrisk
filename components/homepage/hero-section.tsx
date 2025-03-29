d291b3c2f2862be35ed762ca760c41ce858fbca1
Adam Young <adam@mydrones.com.au>
2025-03-29 12:41:40 +1000

    wrong git

--- components/homepage/hero-section.tsx
+++ components/homepage/hero-section.tsx
@@ -22,7 +22,7 @@ export default function HeroSection() {
 					<div className="relative mx-auto max-w-5xl px-6 py-28 lg:py-24">
 						<div className="relative z-10 mx-auto max-w-2xl text-center">
 							<h1 className="text-balance text-4xl font-semibold md:text-5xl lg:text-6xl">
-								Simplifying your Risk Management
+								Modern Software testing reimagined
 							</h1>
 							<p className="mx-auto my-8 max-w-2xl text-xl">
 								Officiis laudantium excepturi ducimus rerum
